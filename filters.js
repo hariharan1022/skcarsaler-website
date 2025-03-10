16e1ff847e5f27a1d90616f0bc189adfb7e86c6e
hariharan1022 <hariharanmahesh34@gmail.com>
2025-03-10 11:53:29 +0530

    Add files via upload

---
+++ filters.js
@@ -0,0 +1,11 @@
+<script>
+    document.getElementById('make').addEventListener('change', function() {
+        var selectedMake = this.value;
+        // Here, you would filter the car listings based on the selected make
+    });
+
+    document.getElementById('price').addEventListener('input', function() {
+        var selectedPrice = this.value;
+        // Filter cars based on the selected price
+    });
+</script>
